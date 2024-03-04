d8b2a990dcf6f482453c06784e3e2274a887af80
Rahul Rajput <rr611305@gmail.com>
2024-03-04 21:43:17 +0530

    clients page complete

---
+++ src/app/(dashboard)/clients/ClientsColumns.jsx
@@ -0,0 +1,101 @@
+"use client";
+import ConfirmAction from "@/components/Modals/ConfirmAction";
+import { DataTableColumnHeader } from "@/components/table/DataTableColumnHeader";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Edit3, MoreVertical, Trash2 } from "lucide-react";
+
+export const ClientsColumns = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "name",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="NAME" />
+    ),
+  },
+  {
+    accessorKey: "vendor",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="VENDOR" />
+    ),
+  },
+  {
+    accessorKey: "address",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="ADDRESS" />
+    ),
+    cell: ({ row }) => {
+      const address = row.original.address;
+      return <p className="truncate">{address}</p>;
+    },
+  },
+  {
+    accessorKey: "phone",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="PHONE" />
+    ),
+  },
+  {
+    accessorKey: "email",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="EMAIL" />
+    ),
+  },
+  {
+    accessorKey: "gst",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="GST (%)" />
+    ),
+  },
+  {
+    id: "actions",
+    enableHiding: false,
+    cell: ({ row }) => {
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreVertical className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end" className="max-w-fit">
+            <DropdownMenuItem className="gap-2 justify-center cursor-pointer text-zinc-900">
+              <Edit3 size={12} />
+              Edit
+            </DropdownMenuItem>
+
+            <ConfirmAction />
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
---
+++ src/app/(dashboard)/clients/page.jsx
@@ -0,0 +1,35 @@
+"use client";
+import AddModal from "@/components/Modals/AddModal";
+import SubHeader from "@/components/Sub-header";
+import Wrapper from "@/components/Wrapper";
+import { DataTable } from "@/components/table/data-table";
+import { Button } from "@/components/ui/button";
+import { Layers2 } from "lucide-react";
+import React, { useState } from "react";
+import { ClientsColumns } from "./ClientsColumns";
+
+const ClientPage = () => {
+  const [clients, setClients] = useState([
+    {
+      name: "John Doe",
+      vendor: "Origami Pvt. Ltd.",
+      address:
+        "Akshya Nagar 1st Block 1st Cross, Rammurthy nagar, Bangalore-560016",
+      phone: "+91 98765 43210",
+      email: "johndoe@gmail.com",
+      gst: "44",
+    },
+  ]);
+  return (
+    <Wrapper>
+      <SubHeader name={"Client List"}>
+        <div className="flex items-center justify-center gap-4">
+          <AddModal type={"Client"} />
+        </div>
+      </SubHeader>
+      <DataTable columns={ClientsColumns} data={clients} />
+    </Wrapper>
+  );
+};
+
+export default ClientPage;
