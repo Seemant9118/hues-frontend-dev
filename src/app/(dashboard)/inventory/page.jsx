3f60b6d1233e8fbba88f0698549b78e5be37420c
Rahul Rajput <rr611305@gmail.com>
2024-03-06 18:36:52 +0530

    dnd added for upload inventory

--- src/app/(dashboard)/inventory/page.jsx
+++ src/app/(dashboard)/inventory/page.jsx
@@ -5,8 +5,17 @@ import Wrapper from "@/components/Wrapper";
 import { Columns } from "@/components/columns";
 import { DataTable } from "@/components/table/data-table";
 import { Button } from "@/components/ui/button";
-import { Layers2, Upload } from "lucide-react";
+import {
+  Check,
+  FileEdit,
+  Layers2,
+  Trash2,
+  Upload,
+  UploadCloud,
+} from "lucide-react";
+import Link from "next/link";
 import React, { useState } from "react";
+import { FileUploader } from "react-drag-drop-files";
 
 const InventoryPage = () => {
   const [products, setProducts] = useState([
@@ -20,14 +29,24 @@ const InventoryPage = () => {
     },
   ]);
   const [isAdding, setIsAdding] = useState(false);
+  const [isUploading, setisUploading] = useState(false);
+  const [files, setFiles] = useState([]);
+
+  const handleChange = async (file) => {
+    setFiles((prev) => [...prev, file]);
+  };
 
   return (
     <>
-      {!isAdding && (
+      {!isAdding && !isUploading && (
         <Wrapper>
           <SubHeader name={"Inventory"}>
             <div className="flex items-center justify-center gap-4">
-              <Button variant={"blue_outline"} size="sm">
+              <Button
+                onClick={() => setisUploading(true)}
+                variant={"blue_outline"}
+                size="sm"
+              >
                 <Upload size={14} />
                 Upload List
               </Button>
@@ -53,6 +72,76 @@ const InventoryPage = () => {
           name={"Add Product"}
           cta={"Product"}
         />
+      )}
+      {isUploading && (
+        <Wrapper className={"justify-start items-center"}>
+          <FileUploader
+            handleChange={handleChange}
+            name="file"
+            types={["xls", "csv"]}
+          >
+            <div className=" min-w-[700px] grow px-5 py-10 flex gap-3 justify-between items-center rounded border-2 border-sky-300 border-dashed border-spacing-3 cursor-pointer">
+              <div className="flex items-center gap-4">
+                <UploadCloud className="text-sky-500" size={40} />
+                <div className="flex flex-col gap-1 ">
+                  <p className=" text-darkText font-medium text-xs">
+                    Select a file or drag and drop here
+                  </p>
+                  <p className="text-grey text-xs font-normal">
+                    File size no more than 10MB
+                  </p>
+                </div>
+              </div>
+              <Button variant="blue_outline">
+                <Upload />
+                Select From Local
+              </Button>
+            </div>
+          </FileUploader>
+          {files.map((file, idx) => (
+            <div
+              key={idx}
+              className="p-4 border-neutral-300 border rounded-sm flex items-center justify-between gap-4 min-w-[700px]"
+            >
+              <div className="flex items-center gap-4">
+                <p className="text-xs font-medium leading-[18px]">
+                  {file.name}
+                </p>
+                <div className="w-1 h-1 rounded-full bg-neutral-400"></div>
+                <a
+                  href="#"
+                  className="text-blue-500 underline underline-offset-2 text-xs leading-4"
+                >
+                  Preview
+                </a>
+                <div className="flex items-center gap-2">
+                  <div className="p-2 rounded-full bg-green-500/10 text-green-500">
+                    <Check size={10} />
+                  </div>
+                  <p className="text-xs font-medium text-green-500 leading-5">
+                    Upload Successfully!
+                  </p>
+                </div>
+              </div>
+              <button
+                onClick={() => {
+                  setFiles((prev) => {
+                    const updated = [...prev];
+                    updated.splice(idx, 1);
+                    return updated;
+                  });
+                }}
+              >
+                <Trash2 className="text-grey" size={14} />
+              </button>
+            </div>
+          ))}
+          <div className="h-[1px] w-full bg-neutral-300 mt-auto"></div>
+
+          <div className="flex justify-end self-end ">
+            <Button onClick={() => setisUploading(false)}>Done</Button>
+          </div>
+        </Wrapper>
       )}
     </>
   );
