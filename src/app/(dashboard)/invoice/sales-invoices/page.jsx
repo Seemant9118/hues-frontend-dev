c73b5c1efc03b5e10f5931ac57c17273309bc401
Rahul Rajput <rr611305@gmail.com>
2024-03-11 14:09:50 +0530

    FIX : create invoice form

--- src/app/(dashboard)/invoice/sales-invoices/page.jsx
+++ src/app/(dashboard)/invoice/sales-invoices/page.jsx
@@ -1,59 +1,55 @@
 "use client";
 import AddProduct from "@/components/AddProduct";
+import CreateSales from "@/components/CreateSales";
 import SubHeader from "@/components/Sub-header";
 import Wrapper from "@/components/Wrapper";
 import { Columns } from "@/components/columns";
 import { DataTable } from "@/components/table/data-table";
 import { Button } from "@/components/ui/button";
-import {Layers2} from "lucide-react";
+import { Layers2 } from "lucide-react";
 import React, { useState } from "react";
 
-
 export default function SalesInvoices() {
-    const [products, setProducts] = useState([
-        {
-            name: "Brand: Crocin",
-            code: "#HUESGT45",
-            description:
-                "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration.",
-            category: "Business Planning",
-            quantity: "44",
-        },
-    ]);
+  const [products, setProducts] = useState([
+    {
+      name: "Brand: Crocin",
+      code: "#HUESGT45",
+      description:
+        "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration.",
+      category: "Business Planning",
+      quantity: "44",
+    },
+  ]);
 
-    const [isAdding, setIsAdding] = useState(false);
+  const [isAdding, setIsAdding] = useState(false);
 
-    return (
-        <>
-            {/* Main Table listing page */}
-            {!isAdding && (
-                <Wrapper>
-                    <SubHeader name={"Sales Invoices"}>
-                        <div className="flex items-center justify-center gap-4">
-                            <Button
-                                onClick={() => setIsAdding(true)}
-                                variant={"blue_outline"}
-                                size="sm"
-                            >
-                                <Layers2 size={14} />
-                                Generate Sales Invoice
-                            </Button>
-                        </div>
-                    </SubHeader>
-                    <DataTable columns={Columns} data={products} />
-                </Wrapper>
-            )}
-            {/* Generate Modal Handling */}
-            {isAdding && (
-                <AddProduct
-                    onCancel={() => setIsAdding(false)}
-                    onSubmit={(newProduct) => {
-                        setIsAdding(false);
-                    }}
-                    name={"Generate Sales Invoice"}
-                    cta={"Sales Invoice"}
-                />
-            )}
-        </>
-    );
-};
+  return (
+    <>
+      {/* Main Table listing page */}
+      {!isAdding && (
+        <Wrapper>
+          <SubHeader name={"Sales Invoices"}>
+            <div className="flex items-center justify-center gap-4">
+              <Button
+                onClick={() => setIsAdding(true)}
+                variant={"blue_outline"}
+                size="sm"
+              >
+                <Layers2 size={14} />
+                Generate Sales Invoice
+              </Button>
+            </div>
+          </SubHeader>
+          <DataTable columns={Columns} data={products} />
+        </Wrapper>
+      )}
+      {/* Generate Modal Handling */}
+      {isAdding && (
+        <CreateSales
+          name="Create Invoice"
+          onCancel={() => setIsAdding(false)}
+        />
+      )}
+    </>
+  );
+}
