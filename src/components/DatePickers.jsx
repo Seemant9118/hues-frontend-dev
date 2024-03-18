40336ee7ded559ca346b35f0fc2de9b051c321ff
Seemant <seemantkamlapuri88@bbdnitm.ac.in>
2024-03-18 19:01:51 +0530

    datepicker issue resolved in profileDetailForm

---
+++ src/components/DatePickers.jsx
@@ -0,0 +1,60 @@
+"use client";
+
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+export default function DatePickers() {
+    const [startDate, setStartDate] = useState('');
+    const years = Array.from({ length: new Date().getFullYear() - 1989 }, (_, i) => 1990 + i);
+    const months = [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+
+    return (
+        <DatePicker
+            className="w-[360px] focus:outline-none absolute top-1/2 -translate-y-2/3 z-20 bg-transparent cursor-pointer"
+            placeholderText="mm/dd/yyyy"
+            popperPlacement="top-right"
+            renderCustomHeader={({ date, changeYear, changeMonth, decreaseMonth, increaseMonth, prevMonthButtonDisabled, nextMonthButtonDisabled }) => (
+                <div style={{ margin: 10, display: "flex", justifyContent: "center" }}>
+                    <button className="text-lg font-bold p-1" onClick={decreaseMonth} disabled={prevMonthButtonDisabled}>
+                        {"<"}
+                    </button>
+                    <select
+                        className="border rounded-md m-1 focus:outline-none"
+                        value={date.getFullYear()}
+                        onChange={({ target: { value } }) => changeYear(Number(value))}
+                    >
+                        {years.map((option) => (
+                            <option key={option} value={option} className="text-sm">
+                                {option}
+                            </option>
+                        ))}
+                    </select>
+
+                    <select
+                        className="border rounded-md m-1 focus:outline-none"
+                        value={months[date.getMonth()]}
+                        onChange={({ target: { value } }) =>
+                            changeMonth(months.indexOf(value))
+                        }
+                    >
+                        {months.map((option) => (
+                            <option key={option} value={option}  className="text-sm">
+                                {option}
+                            </option>
+                        ))}
+                    </select>
+
+                    <button className="text-lg font-bold p-1" onClick={increaseMonth} disabled={nextMonthButtonDisabled}>
+                        {">"}
+                    </button>
+                </div>
+            )}
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+        />
+    );
+};
