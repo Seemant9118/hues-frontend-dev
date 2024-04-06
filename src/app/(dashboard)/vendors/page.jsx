4653ddd8636e8a39739eb3575640db1e2b23d55c
Seemant <seemantkamlapuri88@bbdnitm.ac.in>
2024-04-06 14:23:40 +0530

    cliend,vendor flow bug is resolved

--- src/app/(dashboard)/vendors/page.jsx
+++ src/app/(dashboard)/vendors/page.jsx
@@ -6,9 +6,11 @@ import { DataTable } from "@/components/table/data-table";
 import React, { useState } from "react";
 import { VendorsColumns } from "./VendorsColumns";
 import EmptyStageComponent from "@/components/EmptyStageComponent";
-import { DatabaseZap, FileCheck, FileText, KeySquare } from "lucide-react";
+import { BookUser, Settings, Eye, HeartHandshake } from 'lucide-react';
+
 
 const VendorsPage = () => {
+  const [isOpen,setIsOpen] = useState(false);
   const [vendors, setVendors] = useState([]);
 
   const VendorsEmptyStageData = {
@@ -16,52 +18,25 @@ const VendorsPage = () => {
     catalogs for efficient transactions."`,
     subHeading: "Features",
     subItems: [
-      {
-        id: 1,
-        icon: <FileCheck size={14} />,
-        subItemtitle: `Register vendors with essential details easily.`,
-      },
-      {
-        id: 2,
-        icon: <FileText size={14} />,
-        subItemtitle: `Automatically access vendor catalogs within your purchasing workflow.`,
-      },
-      {
-        id: 3,
-        icon: <KeySquare size={14} />,
-        subItemtitle: `Leverage vendor visibility for informed bids and streamlined purchases`,
-      },
-      {
-        id: 4,
-        icon: <DatabaseZap size={14} />,
-        subItemtitle: `Foster robust vendor relationships with tailored product engagement.`,
-      },
+      { id: 1, icon: <BookUser size={14} />, subItemtitle: `Register vendors with essential details easily.` },
+      { id: 2, icon: <Settings size={14} />, subItemtitle: `Automatically access vendor catalogs within your purchasing workflow.` },
+      { id: 3, icon: <Eye size={14} />, subItemtitle: `Leverage vendor visibility for informed bids and streamlined purchases` },
+      { id: 4, icon: <HeartHandshake size={14} />, subItemtitle: `Foster robust vendor relationships with tailored product engagement.` },
     ],
   };
   return (
     <Wrapper>
       <SubHeader name={"Vendors"}>
         <div className="flex items-center justify-center gap-4">
-          <AddModal
-            type={"Add"}
-            cta="Add"
-            modalHead="Vendor"
-            onSubmit={(newVendors) =>
-              setVendors((vendors) => [...vendors, newVendors])
-            }
-          />
+          <AddModal isOpen={isOpen} setIsOpen={setIsOpen} type={"Add Vendor"} cta="Add" modalHead="Vendor" onSubmit={(newVendors) =>
+            setVendors(vendors => [...vendors, newVendors])
+          } />
         </div>
       </SubHeader>
-      {vendors.length === 0 ? (
-        <EmptyStageComponent
-          heading={VendorsEmptyStageData.heading}
-          desc={VendorsEmptyStageData.desc}
-          subHeading={VendorsEmptyStageData.subHeading}
-          subItems={VendorsEmptyStageData.subItems}
-        />
-      ) : (
-        <DataTable columns={VendorsColumns} data={vendors} />
-      )}
+      {
+        vendors.length === 0 ? <EmptyStageComponent heading={VendorsEmptyStageData.heading} desc={VendorsEmptyStageData.desc} subHeading={VendorsEmptyStageData.subHeading} subItems={VendorsEmptyStageData.subItems} /> :
+          <DataTable columns={VendorsColumns} data={vendors} />
+      }
     </Wrapper>
   );
 };
