585c9b690fb4e17d2551290558ed0523175603fd
Rahul Rajput <rr611305@gmail.com>
2024-05-04 18:49:20 +0530

    sonner package fixes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,7 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { Toaster } from "sonner";
 import QueryWrapper from "@/context/QueryWrapper";
-import { UserProvider } from "@/context/UserContext";
+import { Toaster } from "sonner";
+import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
 
 // Font files can be colocated inside of `app`
 export const metadata = {
@@ -16,12 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`bg-[#F6F9FF] flex flex-col gap-5 min-h-screen max-h-screen`}
+        className={`bg-[#F6F9FF] flex flex-col gap-5 h-screen`}
       >
-        <QueryWrapper>
-          <Toaster richColors position="top-right" />
-          {children}
-        </QueryWrapper>
+      <Toaster richColors position="top-right" />
+        <QueryWrapper>{children}</QueryWrapper>
       </body>
     </html>
   );
