7045535aba21c0102ae191ba76abf32cdeb35cae
Code-X-Rahul <rr611305@gmail.com>
2024-05-27 17:04:28 +0530

    Suspense Added in login page

--- src/app/(auth)/login/multi-step-forms/IndexForm.jsx
+++ src/app/(auth)/login/multi-step-forms/IndexForm.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import Image from "next/image";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
