6a040ce058734095c6ab04c6971f3be7480d890c
Code-X-Rahul <rr611305@gmail.com>
2024-05-31 18:06:21 +0530

    Forward Ref added in input wrapper

--- src/components/Form/wrappers/InputWrappers.jsx
+++ src/components/Form/wrappers/InputWrappers.jsx
@@ -1,11 +1,10 @@
 import { cn } from "@/lib/utils";
+import { forwardRef } from "react";
 
-const InputWrapper = ({
-  children,
-  className,
-}) => {
+const InputWrapper = forwardRef(({ children, className }, ref) => {
   return (
     <div
+      ref={ref}
       className={cn(
         "py-4 px-8 flex flex-col bg-white shadow-[0px_2px_8px_0px_rgba(21,151,212,0.16)]",
         className
@@ -14,6 +13,6 @@ const InputWrapper = ({
       {children}
     </div>
   );
-};
+});
 
 export default InputWrapper;
