0cc14902fa1c582e7e3b5e727f12eed843a4850e
Ritik Sachan <sachanritik1@gmail.com>
2024-06-18 17:48:21 +0530

    DashCard unit test added

---
+++ __tests__/DashCard.test.js
@@ -0,0 +1,46 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import DashCard from '@/components/ui/DashCard.jsx';
+
+describe('DashCard', () => {
+  const title = 'Title';
+  const numbers = '500';
+  const growth = '+10';
+  const icon = <>Icon</>;
+
+  beforeEach(() => {
+    render(
+      <DashCard title={title} numbers={numbers} growth={growth} icon={icon} />,
+    );
+  });
+
+  test('renders title correctly', () => {
+    const dashCardElement = screen.getByText(title).parentElement.parentElement;
+    expect(dashCardElement).toHaveTextContent(title);
+  });
+
+  test('renders numbers correctly', () => {
+    const dashCardElement = screen.getByText(numbers);
+    expect(dashCardElement).toBeInTheDocument();
+  });
+
+  test('renders growth correctly', () => {
+    const growthElement = screen.getByText(growth);
+    expect(growthElement).toBeInTheDocument();
+    expect(growthElement).toHaveClass(
+      'rounded-full px-2 py-1 text-sm font-semibold',
+    );
+    expect(growthElement).toHaveClass('bg-[#E3F4E3] text-[#1EC57F]');
+  });
+
+  test('renders icon correctly', () => {
+    const iconElement = screen.getByText('Icon');
+    expect(iconElement).toBeInTheDocument();
+  });
+
+  test('renders link correctly', () => {
+    const linkElement = screen.getByRole('link');
+    expect(linkElement).toBeInTheDocument();
+    expect(linkElement).toHaveAttribute('href', '/insights');
+  });
+});
