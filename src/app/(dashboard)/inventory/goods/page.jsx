0109fa4b14eb7a6bd1fce87412dd13f4d3580827
Seemant <seemantkamlapuri88@bbdnitm.ac.in>
2024-06-20 14:01:02 +0530

    code refactoring in goods/services making component of UploadItems

--- src/app/(dashboard)/inventory/goods/page.jsx
+++ src/app/(dashboard)/inventory/goods/page.jsx
@@ -17,20 +17,17 @@ import {
 } from '@/services/Inventories_Services/Goods_Inventories/Goods_Inventories';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 import {
-  Check,
   CircleFadingPlus,
   DatabaseZap,
-  Download,
   FileCheck,
   FileText,
   KeySquare,
   Share2,
   Upload,
-  UploadCloud,
 } from 'lucide-react';
 import { useState } from 'react';
-import { FileUploader } from 'react-drag-drop-files';
 import { toast } from 'sonner';
+import UploadItems from '@/components/inventory/UploadItems';
 import { useGoodsColumns } from './GoodsColumns';
 
 function Goods() {
@@ -180,108 +177,13 @@ function Goods() {
       )}
 
       {isUploading && (
-        <Wrapper className={'items-center justify-start'}>
-          <FileUploader
-            handleChange={uploadFile}
-            name="file"
-            types={['xlsx', 'csv']}
-          >
-            <div className="mb-2 flex min-w-[700px] grow border-spacing-3 cursor-pointer items-center justify-between gap-3 rounded border-2 border-dashed border-sky-300 px-5 py-10">
-              <div className="flex items-center gap-4">
-                <UploadCloud className="text-sky-500" size={40} />
-                <div className="flex flex-col gap-1">
-                  <p className="text-xs font-medium text-darkText">
-                    Drag & Drop or Select a File (Max 10MB,
-                    <span className="font-bold text-sky-500">
-                      {' '}
-                      .csv/.xlsx Formats
-                    </span>{' '}
-                    )
-                  </p>
-                  <p className="text-xs font-normal text-sky-500">
-                    Note - Trade enabled for eSigned inventories only.
-                  </p>
-                  {/* <p className="text-sky-500 text-xs font-normal">
-                    Supported File formats : .csv | .xls
-                  </p> */}
-                </div>
-              </div>
-              <Button variant="blue_outline">
-                <Upload />
-                Select
-              </Button>
-            </div>
-          </FileUploader>
-
-          <Button asChild variant="outline" className="w-full max-w-[700px]">
-            <a
-              download={'/ProductGoodsSample.xlsx'}
-              href="/ProductGoodsSample.xlsx"
-            >
-              <Download />
-              Sample
-            </a>
-          </Button>
-          {files.map((file) => (
-            <div
-              key={file.name}
-              className="flex min-w-[700px] items-center justify-between gap-4 rounded-sm border border-neutral-300 p-4"
-            >
-              <div className="flex items-center gap-4">
-                <p className="text-xs font-medium leading-[18px]">
-                  {file.name}
-                </p>
-                <div className="h-1 w-1 rounded-full bg-neutral-400"></div>
-                {/* <a
-                  href="#"
-                  className="text-blue-500 underline underline-offset-2 text-xs leading-4"
-                >
-                  Preview
-                </a> */}
-                <div className="flex items-center gap-2">
-                  <div className="rounded-full bg-green-500/10 p-2 text-green-500">
-                    <Check size={10} />
-                  </div>
-                  <p className="text-xs font-medium leading-5 text-green-500">
-                    Upload Successfully!
-                  </p>
-                </div>
-              </div>
-              <Button
-                variant="ghost"
-                onClick={() => {
-                  setisUploading(false);
-                  setFiles([]);
-                }}
-              >
-                Back to View
-              </Button>
-              {/* <button
-                onClick={() => {
-                  setFiles((prev) => {
-                    const updated = [...prev];
-                    updated.splice(idx, 1);
-                    return updated;
-                  });
-                }}
-              >
-                <Trash2 className="text-grey" size={14} />
-              </button> */}
-            </div>
-          ))}
-          <div className="mt-auto h-[1px] w-full bg-neutral-300"></div>
-
-          {/* <div className="flex justify-end self-end">
-            <Button
-              onClick={() => {
-                setisUploading(false);
-                setFiles([]);
-              }}
-            >
-              Done
-            </Button>
-          </div> */}
-        </Wrapper>
+        <UploadItems
+          type="goods"
+          uploadFile={uploadFile}
+          files={files}
+          setisUploading={setisUploading}
+          setFiles={setFiles}
+        />
       )}
     </>
   );
