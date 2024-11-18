51e15789ef0630007e5eda4ebcd7d9ec3e3fb986
Code-X-Rahul <rr611305@gmail.com>
2024-11-18 17:13:01 +0530

    Fix: Missing Suspense boundary with useSearchParams

--- src/app/(auth)/login/mobileLogin/page.jsx
+++ src/app/(auth)/login/mobileLogin/page.jsx
@@ -1,6 +1,7 @@
 'use client';
 
-import React, { useState } from 'react';
+import Loading from '@/components/ui/Loading';
+import React, { Suspense, useState } from 'react';
 import MobileLogin from '../multi-step-forms/mobileLoginOTPComponents/MobileLogin';
 import VerifyMobileOTP from '../multi-step-forms/mobileLoginOTPComponents/VerifyMobileOTP';
 
@@ -8,14 +9,16 @@ const MobileLoginPage = () => {
   const [mobileLoginStep, setMobileLoginStep] = useState(1);
 
   return (
-    <div className="flex h-full items-center justify-center">
-      {mobileLoginStep === 1 && (
-        <MobileLogin setMobileLoginStep={setMobileLoginStep} />
-      )}
-      {mobileLoginStep === 2 && (
-        <VerifyMobileOTP setMobileLoginStep={setMobileLoginStep} />
-      )}
-    </div>
+    <Suspense fallback={<Loading />}>
+      <div className="flex h-full items-center justify-center">
+        {mobileLoginStep === 1 && (
+          <MobileLogin setMobileLoginStep={setMobileLoginStep} />
+        )}
+        {mobileLoginStep === 2 && (
+          <VerifyMobileOTP setMobileLoginStep={setMobileLoginStep} />
+        )}
+      </div>
+    </Suspense>
   );
 };
 
