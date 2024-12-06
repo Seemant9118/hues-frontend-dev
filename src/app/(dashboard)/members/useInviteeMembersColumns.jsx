120d7802a0e7e7c98a2b96154711980339c1a13f
Seemant <seemantkamlapuri88@bbdnitm.ac.in>
2024-12-06 17:02:19 +0530

    feat : Show invite link in members menu to share to associates after they have been invited is done

--- src/app/(dashboard)/members/useInviteeMembersColumns.jsx
+++ src/app/(dashboard)/members/useInviteeMembersColumns.jsx
@@ -1,7 +1,9 @@
 'use client';
 
+import GenerateLink from '@/components/enterprise/GenerateLink';
 import { DataTableColumnHeader } from '@/components/table/DataTableColumnHeader';
 import { Checkbox } from '@/components/ui/checkbox';
+import { generateLink } from '@/services/Invitation_Service/Invitation_Service';
 import moment from 'moment';
 
 export const useInviteeMembersColumns = (setSelectedOrders) => {
@@ -117,6 +119,23 @@ export const useInviteeMembersColumns = (setSelectedOrders) => {
           <div className="w-fit rounded-[5px] border border-[#EDEEF2] bg-[#F6F7F9] p-1 text-sm">
             {capitalize(role)}
           </div>
+        );
+      },
+    },
+    {
+      accessorKey: 'invitation',
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="INVITATION" />
+      ),
+      cell: ({ row }) => {
+        const { id, status } = row.original.invitation;
+
+        return (
+          <GenerateLink
+            invitationStatus={status}
+            invitationId={id}
+            mutationFunc={generateLink}
+          />
         );
       },
     },
