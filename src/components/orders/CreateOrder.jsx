2f701b221fdb445dfc615f914b8242c45b29c3b6
Seemant <seemantkamlapuri88@bbdnitm.ac.in>
2025-01-02 17:58:37 +0530

    fix : option for creating new client,vendor in order creation flow.

--- src/components/orders/CreateOrder.jsx
+++ src/components/orders/CreateOrder.jsx
@@ -523,12 +523,6 @@ const CreateOrder = ({
                     {type === 'sales' && searchCustomerData?.length === 0 && (
                       <div className="flex flex-col items-center gap-1 py-2 text-xs">
                         <span>Client Not Found</span>
-                        <AddModal
-                          type={'Add Client'}
-                          cta="client"
-                          btnName="Add Client"
-                          mutationFunc={CreateEnterpriseUser}
-                        />
                       </div>
                     )}
                     <>
@@ -549,6 +543,12 @@ const CreateOrder = ({
                             customer.invitation?.userDetails?.name}
                         </SelectItem>
                       ))}
+                      <AddModal
+                        type={'Add Client'}
+                        cta="client"
+                        btnName="Add a new Client"
+                        mutationFunc={CreateEnterpriseUser}
+                      />
                     </>
                   </SelectContent>
                 </Select>
@@ -591,12 +591,6 @@ const CreateOrder = ({
                   {type === 'purchase' && searchCustomerData?.length === 0 && (
                     <div className="flex flex-col items-center gap-1 py-2 text-xs">
                       <span>Vendor Not Found</span>
-                      <AddModal
-                        type={'Add Vendor'}
-                        cta="vendor"
-                        btnName="Add"
-                        mutationFunc={CreateEnterpriseUser}
-                      />
                     </div>
                   )}
 
@@ -619,6 +613,12 @@ const CreateOrder = ({
                           {customer?.vendor?.name}
                         </SelectItem>
                       ))}
+                    <AddModal
+                      type={'Add Vendor'}
+                      cta="vendor"
+                      btnName="Add a new Vendor"
+                      mutationFunc={CreateEnterpriseUser}
+                    />
                   </>
                 </SelectContent>
               </Select>
