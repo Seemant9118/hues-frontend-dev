898b5451154ea9d4d6e136d163b8ec980d14356a
Abhinn Krishn <abhinnk21@gmail.com>
2025-03-26 17:13:15 +0530

    test: add export test

--- __tests__/e2e/sales/orders.spec.ts
+++ __tests__/e2e/sales/orders.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import fs from 'fs';
 
 test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:3000/en/');
@@ -47,6 +48,38 @@ test('Can filter Test', async ({ page }) => {
   await expect(
     page.getByRole('cell', { name: 'ORD/G0IO49/2425/' }),
   ).toBeVisible();
+});
+
+test('CSV export should contain data', async ({ page }) => {
+  await page
+    .getByRole('row', {
+      name: 'Select row ORD/G0IO49/2425/0084 19-03-2025 B2B Dummy Vendor 1741376093942',
+    })
+    .getByLabel('Select row')
+    .click();
+
+  // wait for download event
+  const downloadPromise = page.waitForEvent('download');
+  await page.locator('button:has(svg.lucide-upload)').click();
+  // Wait for the download to complete
+  const download = await downloadPromise;
+
+  // Ensure the file has been downloaded
+  expect(download.suggestedFilename()).toContain('.xlsx');
+
+  // Save the file locally
+  const filePath = `./e/Download/${download.suggestedFilename()}`;
+  await download.saveAs(filePath);
+
+  // Read the file contents
+  const content = fs.readFileSync(filePath, 'utf-8').trim();
+
+  // Ensure the file has data (not just headers)
+  expect(content).not.toBe('');
+  expect(content.split('\n').length).toBeGreaterThan(1); // More than just a header
+
+  // Optionally, delete the file after the test
+  fs.unlinkSync(filePath);
 });
 
 test.describe.serial('Offer Management', () => {
