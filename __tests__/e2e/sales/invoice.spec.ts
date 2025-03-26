0a3e04fe58bdb1ad7ba4829d347a652af21a96c6
Abhinn Krishn <abhinnk21@gmail.com>
2025-03-26 17:27:47 +0530

    test(invoice): add can export data test

--- __tests__/e2e/sales/invoice.spec.ts
+++ __tests__/e2e/sales/invoice.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import fs from 'fs';
 
 test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:3000/en/');
@@ -82,4 +83,37 @@ test('Can Click on Tabs Test', async ({ page }) => {
   await expect(page.getByText('INV/G0IO49/2425/0019')).toBeVisible();
   await page.getByRole('tab', { name: 'Disputed' }).click();
   await expect(page.getByText('INV/G0IO49/2425/0001')).toBeVisible();
+});
+
+test('CSV export should contain data', async ({ page }) => {
+  await page.getByRole('tab', { name: 'All' }).click();
+  await page
+    .getByRole('row', {
+      name: 'Select row INV/G0IO49/2425/0019 25-03-2025 B2B Kamlapuri Enterprises Payment',
+    })
+    .getByLabel('Select row')
+    .click();
+
+  // wait for download event
+  const downloadPromise = page.waitForEvent('download');
+  await page.locator('button:has(svg.lucide-upload)').click();
+  // Wait for the download to complete
+  const download = await downloadPromise;
+
+  // Ensure the file has been downloaded
+  expect(download.suggestedFilename()).toContain('.xlsx');
+
+  // Save the file locally
+  const filePath = `./e/Download/${download.suggestedFilename()}`;
+  await download.saveAs(filePath);
+
+  // Read the file contents
+  const content = fs.readFileSync(filePath, 'utf-8').trim();
+
+  // Ensure the file has data (not just headers)
+  expect(content).not.toBe('');
+  expect(content.split('\n').length).toBeGreaterThan(1); // More than just a header
+
+  // Optionally, delete the file after the test
+  fs.unlinkSync(filePath);
 });
