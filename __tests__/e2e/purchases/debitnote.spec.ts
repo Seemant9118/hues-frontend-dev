3d13e9eff291c4b66273a2cc90a6cca6930b7a91
Abhinn Krishn <abhinnk21@gmail.com>
2025-03-31 14:51:30 +0530

    test: user can add new comment in a Debit note

--- __tests__/e2e/purchases/debitnote.spec.ts
+++ __tests__/e2e/purchases/debitnote.spec.ts
@@ -2,19 +2,36 @@ import { test, expect } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:3000/en/');
+  await page.waitForLoadState('networkidle');
+  await page.waitForLoadState('domcontentloaded');
   await page.getByRole('link', { name: 'Purchases' }).click();
+  await page.waitForLoadState('networkidle');
+  await page.waitForLoadState('domcontentloaded');
   await page.getByRole('link', { name: 'Debit Notes' }).click();
+  await page.waitForLoadState('networkidle');
+  await page.waitForLoadState('domcontentloaded');
 });
 
-test('should first', async ({ page }) => {
-  await page.getByText('DBN/G0IO49/2425/').click();
+test('Can add new comment in a Debit note', async ({ page }) => {
+  function generateRandomString(length = 10) {
+    const characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(
+        Math.floor(Math.random() * characters.length),
+      );
+    }
+    return result;
+  }
+
+  const random = generateRandomString();
+
+  await page.getByText('DBN/G0IO49/2425/0003').click();
+  await page.waitForLoadState('networkidle');
+  await page.waitForLoadState('domcontentloaded');
   await page.getByPlaceholder('Type your comment here...').click();
-  await page.getByPlaceholder('Type your comment here...').fill('comment');
-  await page
-    .locator('div')
-    .filter({ hasText: /^comment$/ })
-    .getByRole('img')
-    .nth(2)
-    .click();
-  await expect(page.getByText('comment').nth(1)).toBeVisible();
+  await page.getByPlaceholder('Type your comment here...').fill(random);
+  await page.locator('svg.lucide-arrow-up.cursor-pointer').click();
+  await expect(page.getByText(random, { exact: true })).toBeVisible();
 });
