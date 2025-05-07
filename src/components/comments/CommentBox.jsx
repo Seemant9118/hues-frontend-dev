e7e03d683f5e059738c250ebff5c911e2024ba18
Seemant <seemantkamlapuri88@bbdnitm.ac.in>
2025-05-07 16:45:29 +0530

    feat: Enhance CommentBox with file upload and improved UI for comment submission

--- src/components/comments/CommentBox.jsx
+++ src/components/comments/CommentBox.jsx
@@ -1,14 +1,33 @@
-import { ChevronDown, ChevronUp } from 'lucide-react';
+import {
+  ArrowUp,
+  Building2,
+  ChevronDown,
+  ChevronUp,
+  Paperclip,
+} from 'lucide-react';
 import React, { useState } from 'react';
+import Tooltips from '../auth/Tooltips';
 import { Button } from '../ui/button';
 import {
   Collapsible,
   CollapsibleContent,
   CollapsibleTrigger,
 } from '../ui/collapsible';
+import Loading from '../ui/Loading';
+import { Textarea } from '../ui/textarea';
 
-const CommentBox = () => {
+const CommentBox = ({
+  createCommentMutation,
+  handleSubmitComment,
+  uploadMedia,
+}) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [comment, setComment] = useState({
+    files: [],
+    contextType: '',
+    contextId: null,
+    text: '',
+  });
 
   return (
     <Collapsible
@@ -25,7 +44,7 @@ const CommentBox = () => {
       >
         {!isOpen && (
           <section className="flex w-full animate-fadeInUp items-center justify-between">
-            Comments
+            <h1 className="text-sm font-bold">Comments</h1>
           </section>
         )}
         {isOpen && <h1 className="text-sm font-bold">{'Comments'}</h1>}
@@ -37,7 +56,66 @@ const CommentBox = () => {
         </CollapsibleTrigger>
       </div>
 
-      <CollapsibleContent className="animate-fadeInUp space-y-2"></CollapsibleContent>
+      <CollapsibleContent className="animate-fadeInUp space-y-2">
+        <div className="relative">
+          {/* 1 */}
+          <div className="absolute left-5 top-[15px] flex h-10 w-10 items-center justify-center rounded-full bg-[#A5ABBD]">
+            <Building2 size={20} />
+          </div>
+
+          {/* 2 */}
+          <Textarea
+            name="comment"
+            value={comment.text}
+            onChange={(e) => {
+              setComment((prev) => ({ ...prev, text: e.target.value }));
+            }}
+            className="px-20 pt-[20px]"
+            placeholder={'Type your comment here...'}
+          />
+
+          {/* 3 */}
+          <div className="absolute right-6 top-[24px] flex gap-4 text-[#A5ABBD]">
+            <Tooltips
+              trigger={
+                <label htmlFor="fileUpload">
+                  <Paperclip
+                    size={20}
+                    className="cursor-pointer hover:text-black"
+                  />
+                </label>
+              }
+              content={'attach file'}
+            />
+
+            <input
+              type="file"
+              id="fileUpload"
+              style={{ display: 'none' }}
+              onChange={(e) => {
+                if (e.target.files[0]) {
+                  uploadMedia(e.target.files[0]);
+                }
+              }}
+            />
+
+            <Tooltips
+              trigger={
+                createCommentMutation.isPending ? (
+                  <Loading />
+                ) : (
+                  <ArrowUp
+                    size={20}
+                    onClick={handleSubmitComment}
+                    className={'cursor-pointer hover:text-black'}
+                  />
+                )
+              }
+              content={'send'}
+            />
+          </div>
+        </div>
+      </CollapsibleContent>
     </Collapsible>
   );
 };
