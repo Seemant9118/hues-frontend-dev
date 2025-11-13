2f98010a6881a48b8f7abfe3089ef0f3f89c81da
Seemant Kamlapuri <seemantkamlapuri88@gmail.com>
2025-11-13 13:53:34 +0530

    refactor: replace redirectToHomeWithFcm with goToHomePage for consistent navigation handling

--- src/components/Popovers/ProfileInfoPopUp.jsx
+++ src/components/Popovers/ProfileInfoPopUp.jsx
@@ -12,9 +12,7 @@ import {
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { ShieldBan, User } from 'lucide-react';
 import { useTranslations } from 'next-intl';
-
 import { AdminAPIs } from '@/api/adminApi/AdminApi';
-import { redirectToHomeWithFcm } from '@/appUtils/redirectionUtilFn';
 import { goToHomePage } from '@/appUtils/redirectionUtilFn';
 import { usePathname, useRouter } from '@/i18n/routing';
 import { revertSwitchedEnterprise } from '@/services/Admin_Services/AdminServices';
