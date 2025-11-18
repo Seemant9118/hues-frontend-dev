f2ce9d4ae12eb77dffd21322bfcb09534159f807
Seemant Kamlapuri <seemantkamlapuri88@gmail.com>
2025-11-18 13:09:27 +0530

    feat: change the stucture of roles and permisions

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -7,16 +7,18 @@ const AuthContext = createContext({
 });
 
 export const AuthProvider = ({ children }) => {
+  const [name, setName] = useState('');
   const [role, setRole] = useState(null);
   const [permissions, setPermissions] = useState([]); // now array of permission objects
 
-  const setAuthData = (userRole, permissionArray) => {
+  const setAuthData = (userName, userRole, permissionArray) => {
+    setName(userName);
     setRole(userRole);
     setPermissions(permissionArray);
   };
 
   return (
-    <AuthContext.Provider value={{ role, permissions, setAuthData }}>
+    <AuthContext.Provider value={{ name, role, permissions, setAuthData }}>
       {children}
     </AuthContext.Provider>
   );
