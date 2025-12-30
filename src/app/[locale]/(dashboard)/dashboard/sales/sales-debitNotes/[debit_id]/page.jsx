52156c8e3f528887a06445ac081fc99b09ccedd4
Seemant Kamlapuri <seemantkamlapuri88@gmail.com>
2025-12-30 17:59:10 +0530

    feat: Implement debit note response functionality and enhance UI components
    - Added `debitnoteColumns.jsx` for managing debit note item columns in the dashboard.
    - Updated `page.jsx` to integrate new debit note columns and handle response actions.
    - Created `respondDebitNoteItemsColumns.jsx` for response-specific columns in debit notes.
    - Enhanced `useDebitNotesColumns.jsx` to include additional fields and improve data representation.
    - Introduced `CreateResponse.jsx` component for handling seller responses to debit notes.
    - Modified `DebitNoteServices.js` to include API call for seller responses.
    - Improved `InvoiceOverview.jsx` layout for better readability.

--- src/app/[locale]/(dashboard)/dashboard/sales/sales-debitNotes/[debit_id]/page.jsx
+++ src/app/[locale]/(dashboard)/dashboard/sales/sales-debitNotes/[debit_id]/page.jsx
@@ -1,41 +1,35 @@
-/* eslint-disable import/no-unresolved */
-
 'use client';
 
 import { DebitNoteApi } from '@/api/debitNote/DebitNoteApi';
-import { capitalize, formattedAmount } from '@/appUtils/helperFunctions';
-import Tooltips from '@/components/auth/Tooltips';
-import Comment from '@/components/comments/Comment';
-import DebitNoteModal from '@/components/Modals/DebitNoteModal';
+import {
+  formattedAmount,
+  getQCDefectStatuses,
+} from '@/appUtils/helperFunctions';
+import CommentBox from '@/components/comments/CommentBox';
+import CreateResponse from '@/components/debitNote/CreateResponse';
+import ConditionalRenderingStatus from '@/components/orders/ConditionalRenderingStatus';
 import OrderBreadCrumbs from '@/components/orders/OrderBreadCrumbs';
+import { DataTable } from '@/components/table/data-table';
 import { Button } from '@/components/ui/button';
-import Loading from '@/components/ui/Loading';
-import { Textarea } from '@/components/ui/textarea';
+import Overview from '@/components/ui/Overview';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { ProtectedWrapper } from '@/components/wrappers/ProtectedWrapper';
 import Wrapper from '@/components/wrappers/Wrapper';
 import { useAuth } from '@/context/AuthContext';
 import useMetaData from '@/hooks/useMetaData';
 import { usePermission } from '@/hooks/usePermissions';
 import {
-  createComments,
-  getComments,
   getDebitNote,
+  updateDebitNote,
 } from '@/services/Debit_Note_Services/DebitNoteServices';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import {
-  Building2,
-  Check,
-  FileText,
-  Image,
-  MessageCircle,
-  Paperclip,
-  X,
-} from 'lucide-react';
+import { BookOpen, MoveUpRight, PlusCircle, Save } from 'lucide-react';
 import moment from 'moment';
 import { useTranslations } from 'next-intl';
 import { useParams, useRouter } from 'next/navigation';
 import React, { useState } from 'react';
 import { toast } from 'sonner';
+import { useDebitNoteColumns } from './debitnoteColumns';
 
 const ViewDebitNote = () => {
   useMetaData('Hues! - Debit Notes Details', 'HUES DEBITNOTES'); // dynamic title
@@ -50,13 +44,8 @@ const ViewDebitNote = () => {
   const queryClient = useQueryClient();
   const params = useParams();
   const debitNoteId = params.debit_id;
-  const [files, setFiles] = useState([]);
-  const [comment, setComment] = useState({
-    files: [],
-    contextType: '',
-    contextId: null,
-    text: '',
-  });
+  const [isAddingResponse, setIsAddingResponse] = useState(false);
+  const [tabs, setTabs] = useState('overview');
 
   const debitNoteBreadCrumbs = [
     {
@@ -74,74 +63,138 @@ const ViewDebitNote = () => {
     },
   ];
 
+  const onTabChange = (tab) => {
+    setTabs(tab);
+  };
+
   // get debitNote
-  const { data: debitNote, isLoading } = useQuery({
+  const { data: debitNoteDetails } = useQuery({
     queryKey: [DebitNoteApi.getDebitNote.endpointKey, debitNoteId],
     queryFn: () => getDebitNote(debitNoteId),
     select: (debitNote) => debitNote.data.data,
     enabled: hasPermission('permission:sales-view'),
   });
 
-  // get comments
-  const { data: comments, isLoading: isCommentLoading } = useQuery({
-    queryKey: [DebitNoteApi.getComments.endpointKey, debitNoteId],
-    queryFn: () => getComments(debitNoteId, 'DEBIT_NOTE'),
-    select: (comments) => comments.data.data,
-  });
-
-  const uploadMedia = async (file) => {
-    setFiles((prev) => [...prev, file]);
-    toast.success('File attached successfully!');
+  const overviewData = {
+    debitNoteId: debitNoteDetails?.referenceNumber,
+    clientName: debitNoteDetails?.fromEnterprise?.name,
+    invoiceId: debitNoteDetails?.invoice.referenceNumber,
+    defects: '',
+    claimedAmount: formattedAmount(debitNoteDetails?.amount),
+    setteledAmount: formattedAmount(debitNoteDetails?.setteledAmount),
+    createdOn: moment(debitNoteDetails?.createdAt).format('DD/MM/YYYY'),
+    status: debitNoteDetails?.status,
+  };
+  const overviewLabels = {
+    debitNoteId: translations('overview_labels.debitNoteId'),
+    clientName: translations('overview_labels.clientName'),
+    invoiceId: translations('overview_labels.invoiceId'),
+    defects: translations('overview_labels.defects'),
+    claimedAmount: translations('overview_labels.claimedAmount'),
+    setteledAmount: translations('overview_labels.setteledAmount'),
+    createdOn: translations('overview_labels.createdOn'),
+    status: translations('overview_labels.status'),
   };
 
-  const handleFileRemove = (file) => {
-    setFiles((prevFiles) => prevFiles.filter((f) => f.name !== file.name));
-  };
+  const customRender = {
+    defects: () => {
+      const statuses = getQCDefectStatuses(debitNoteDetails);
 
-  const createCommentMutation = useMutation({
-    mutationKey: [DebitNoteApi.createComments.endpointKey],
-    mutationFn: createComments,
-    onSuccess: () => {
-      toast.success('Comment added successfully!');
-      queryClient.invalidateQueries([
-        DebitNoteApi.getComments.endpointKey,
-        debitNoteId,
-      ]);
-      setComment({
-        files: [],
-        contextType: '',
-        contextId: null,
-        text: '',
-      });
-      setFiles([]);
-    },
-    onError: (error) => {
-      toast.error(
-        error.response.data.message || translations('errorMsg.common'),
+      if (!statuses?.length) return '-';
+
+      return (
+        <div className="flex flex-wrap gap-2">
+          {statuses.map((status) => (
+            <ConditionalRenderingStatus key={status} status={status} isQC />
+          ))}
+        </div>
       );
     },
+    invoiceId: () => {
+      const invoiceId = debitNoteDetails?.invoice.id;
+      const invoiceRef = debitNoteDetails?.invoice.referenceNumber;
+
+      return (
+        <p
+          className={`flex items-center gap-1 ${
+            invoiceId
+              ? 'cursor-pointer hover:text-primary hover:underline'
+              : 'cursor-default text-muted-foreground'
+          }`}
+          onClick={() => {
+            if (invoiceId) {
+              router.push(`/dashboard/sales/sales-invoices/${invoiceId}`);
+            }
+          }}
+        >
+          {invoiceRef ? (
+            <>
+              {invoiceRef}
+              <MoveUpRight size={14} />
+            </>
+          ) : (
+            '--'
+          )}
+        </p>
+      );
+    },
+  };
+
+  // TODO: api change for raise credit note and saved draft
+  const updateDebitNoteMutation = useMutation({
+    mutationFn: updateDebitNote,
+    onSuccess: () => {
+      toast.success('Debit Note updated Successfully');
+      queryClient.invalidateQueries([DebitNoteApi.getDebitNote.endpointKey]);
+    },
+    onError: (error) => {
+      toast.error(error.response.data.message || 'something went wrong');
+    },
   });
 
-  const handleSubmitComment = () => {
-    if (!comment.text.trim()) {
-      toast.error('Comment cannot be empty!');
-      return;
-    }
+  const handleSubmit = (type = 'DRAFT') => {
+    if (!debitNoteDetails?.debitNoteItems?.length) return;
 
-    const formData = new FormData();
-    formData.append('contextType', 'DEBIT_NOTE'); // assuming fixed or dynamic context
-    formData.append('contextId', debitNoteId); // use actual ID here
-    formData.append('text', comment.text);
+    const payload = {
+      status: type, // 'DRAFT' | 'SENT'
+      items: debitNoteDetails.debitNoteItems.map((item) => {
+        const { buyerExpectation } = item;
 
-    // handle files if any
-    if (files.length > 0) {
-      files.forEach((file) => {
-        formData.append('files', file);
-      });
-    }
+        return {
+          debitNoteItemId: item.id,
+          buyerExpectation,
 
-    createCommentMutation.mutate(formData);
+          ...(buyerExpectation === 'REQUEST_REFUND' && {
+            refundAmount: Number(item.amount || 0),
+          }),
+
+          ...(buyerExpectation === 'REQUEST_REPLACEMENT' && {
+            refundAmount: 0,
+            replacementQuantity: Number(item.replacementQuantity || 0),
+          }),
+
+          ...(buyerExpectation === 'REQUEST_BOTH' && {
+            refundAmount: Number(item.amount || 0),
+            replacementQuantity: Number(item.replacementQuantity || 0),
+          }),
+
+          metaData: {
+            internalRemark: item?.metaData?.internalRemark || '',
+            severityIndicator: item?.metaData?.severityIndicator || '',
+          },
+        };
+      }),
+    };
+
+    updateDebitNoteMutation.mutate({
+      id: debitNoteId,
+      data: payload,
+    });
   };
+
+  const debitNoteColumns = useDebitNoteColumns({
+    isDebitNotePosted: debitNoteDetails?.status === 'SENT',
+  });
 
   if (!permissions || permissions.length === 0) {
     return null; // or <Loading />
@@ -155,206 +208,80 @@ const ViewDebitNote = () => {
   return (
     <ProtectedWrapper permissionCode="permission:sales-view">
       <Wrapper className="h-full py-2">
-        <div className="sticky top-0 z-10 flex gap-2 bg-white pt-2">
+        {/* Header */}
+        <section className="sticky top-0 z-10 flex items-center justify-between bg-white py-2">
           {/* breadcrumbs */}
           <OrderBreadCrumbs possiblePagesBreadcrumbs={debitNoteBreadCrumbs} />
-        </div>
-
-        {isLoading && <Loading />}
-        <section className="scrollBarStyles relative flex h-full w-full flex-col gap-4 overflow-y-auto">
-          {/* debitNote overview */}
-          {!isLoading && debitNote && (
-            <div className="sticky top-0 z-10 flex flex-col gap-8 rounded-lg border bg-white px-8 py-4 shadow-customShadow">
-              <section className="flex items-center justify-between">
-                <div className="flex flex-col gap-8">
-                  <h1 className="flex items-center gap-2 text-sm">
-                    <span className="font-bold">
-                      {debitNote.referenceNumber}
-                    </span>
-                    <span className="rounded border border-[#EDEEF2] bg-[#F6F7F9] p-1.5 text-xs">
-                      {capitalize(debitNote?.status)}
-                    </span>
-                  </h1>
-                  <div className="flex gap-10">
-                    <h1 className="text-sm">
-                      <span className="font-bold text-[#ABB0C1]">
-                        {translations('label.date')}:{' '}
-                      </span>
-                      <span className="text-[#363940]">
-                        {moment(debitNote.createdAt).format('DD/MM/YYYY')}
-                      </span>
-                    </h1>
-                    <h1 className="text-sm">
-                      <span className="font-bold text-[#ABB0C1]">
-                        {translations('label.total_amount')} :{' '}
-                      </span>
-                      <span className="font-bold text-[#363940]">
-                        {formattedAmount(debitNote.amount)}
-                      </span>
-                      <span> (inc. GST)</span>
-                    </h1>
-                  </div>
-                </div>
-              </section>
-
-              <h1 className="text-sm">
-                <span className="font-bold text-[#ABB0C1]">
-                  {translations('label.reason')} :{' '}
-                </span>
-                <span className="text-[#363940]">{debitNote.remark}</span>
-              </h1>
-            </div>
-          )}
-
-          {/* comments  */}
-          <div className="flex h-full flex-col gap-4 p-2">
-            <section className="flex w-full items-center gap-2">
-              <MessageCircle size={16} />
-              <h1 className="text-sm font-bold">
-                {translations('comments.title')}
-              </h1>
-            </section>
-
-            <div className="relative">
-              {/* 1 */}
-              <div className="absolute left-5 top-[15px] flex h-10 w-10 items-center justify-center rounded-full border bg-[#A5ABBD]">
-                <Building2 size={20} />
-              </div>
-
-              {/* 2 */}
-              <Textarea
-                name="comment"
-                value={comment.text}
-                onChange={(e) => {
-                  setComment((prev) => ({ ...prev, text: e.target.value }));
-                }}
-                className="px-20 pt-[20px]"
-                placeholder={translations('comments.input.placeholder')}
-              />
-
-              {/* 3 */}
-              <div className="absolute right-6 top-[18px] flex items-center gap-4 text-[#A5ABBD]">
-                <Tooltips
-                  trigger={
-                    <label htmlFor="fileUpload">
-                      <Paperclip
-                        size={20}
-                        className="cursor-pointer hover:text-black"
-                      />
-                    </label>
-                  }
-                  content={translations(
-                    'comments.ctas.attach_file.placeholder',
-                  )}
-                />
-
-                <input
-                  type="file"
-                  id="fileUpload"
-                  style={{ display: 'none' }}
-                  onChange={(e) => {
-                    if (e.target.files[0]) {
-                      uploadMedia(e.target.files[0]);
-                    }
-                  }}
-                />
-
-                <Tooltips
-                  trigger={
-                    createCommentMutation.isPending ? (
-                      <Loading />
-                    ) : (
-                      <Button size="sm" onClick={handleSubmitComment}>
-                        Send
-                      </Button>
-                    )
-                  }
-                  content={translations('comments.ctas.send.placeholder')}
-                />
-              </div>
-            </div>
-
-            <div className="flex flex-col">
-              {/* attached files */}
-              {files?.length > 0 && (
-                <span className="text-xs font-bold">
-                  {translations('comments.attached_files_heading')}
-                </span>
-              )}
-              <div className="flex flex-wrap gap-4">
-                {files?.map((file) => (
-                  <div
-                    key={file.name}
-                    className="relative flex w-64 flex-col gap-2 rounded-xl border border-neutral-300 bg-white p-4 shadow-sm"
-                  >
-                    {/* Remove Button */}
-                    <X
-                      size={16}
-                      onClick={() => handleFileRemove(file)}
-                      className="absolute right-2 top-2 cursor-pointer text-neutral-500 hover:text-red-500"
-                    />
-
-                    {/* File icon */}
-                    <div className="flex h-10 w-10 items-center justify-center rounded-full bg-neutral-100 text-neutral-500">
-                      {file.name.split('.').pop() === 'pdf' ? (
-                        <FileText size={16} className="text-red-600" />
-                      ) : (
-                        // eslint-disable-next-line jsx-a11y/alt-text
-                        <Image size={16} className="text-primary" />
-                      )}
-                    </div>
-
-                    {/* File name */}
-                    <p className="truncate text-sm font-medium text-neutral-800">
-                      {file.name}
-                    </p>
-
-                    {/* Success message */}
-                    <div className="flex items-center gap-2">
-                      <div className="rounded-full bg-green-500/10 p-1.5 text-green-600">
-                        <Check size={12} />
-                      </div>
-                      <p className="text-xs font-medium text-green-600">
-                        {'File attached'}
-                      </p>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </div>
-
-            {/* comments lists */}
-            <section className="flex flex-col gap-2">
-              {isCommentLoading && <Loading />}
-              {!isCommentLoading &&
-                comments?.length > 0 &&
-                comments?.map((comment) => (
-                  <Comment
-                    key={comment?.id}
-                    invalidateId={debitNoteId}
-                    comment={comment}
-                  />
-                ))}
-
-              {!isCommentLoading && comments?.length === 0 && (
-                <div className="flex flex-col items-center justify-center gap-2 rounded-lg bg-gray-50 p-4 text-sm text-[#939090]">
-                  <h1>{translations('comments.emtpyStateComponent.title')}</h1>
-                  <p>{translations('comments.emtpyStateComponent.para')}</p>
-                </div>
-              )}
-            </section>
-          </div>
         </section>
 
-        <ProtectedWrapper permissionCode={'permission:sales-debit-note-action'}>
-          {/* cta's for accept/reject debit note */}
-          {debitNote?.status === 'PENDING' && (
-            <div className="sticky bottom-0 z-10 flex w-full justify-end gap-2 bg-white">
-              <DebitNoteModal cta="reject" debitNote={debitNote} />
-              <DebitNoteModal cta="accept" debitNote={debitNote} />
+        <Tabs
+          value={tabs}
+          onValueChange={onTabChange}
+          defaultValue={'overview'}
+        >
+          <section className="flex items-center justify-between gap-2">
+            <TabsList className="border">
+              <TabsTrigger value="overview">
+                {translations('tabs.tab1.title')}
+              </TabsTrigger>
+            </TabsList>
+
+            {/* TODO: ctas hide logic */}
+            <div className="flex items-center gap-2">
+              <Button
+                disabled={updateDebitNoteMutation?.isPending}
+                size="sm"
+                onClick={() => setIsAddingResponse(true)}
+              >
+                <PlusCircle size={14} /> Add Response
+              </Button>
+              <Button
+                variant="outline"
+                size="sm"
+                disabled={true}
+                onClick={() => handleSubmit('DRAFT')}
+              >
+                <Save size={14} /> Save as Draft
+              </Button>
+              <Button
+                disabled={true}
+                size="sm"
+                onClick={() => handleSubmit('SENT')}
+              >
+                <BookOpen size={14} /> Finalize & Post Credit Note
+              </Button>
             </div>
-          )}
-        </ProtectedWrapper>
+          </section>
+
+          <TabsContent value="overview">
+            <div className="flex flex-col gap-4">
+              {/* OVERVIEW SECTION */}
+              <Overview
+                collapsible={false}
+                data={overviewData}
+                labelMap={overviewLabels}
+                customRender={customRender}
+              />
+
+              {/* comment */}
+              <CommentBox contextId={debitNoteId} context={'DEBIT_NOTE'} />
+
+              <DataTable
+                id="grns"
+                columns={debitNoteColumns}
+                data={debitNoteDetails?.debitNoteItems || []}
+              />
+
+              {/* Create Response comopnent */}
+              <CreateResponse
+                open={isAddingResponse}
+                onOpenChange={setIsAddingResponse}
+                data={debitNoteDetails?.debitNoteItems || []}
+                id={debitNoteId}
+              />
+            </div>
+          </TabsContent>
+        </Tabs>
       </Wrapper>
     </ProtectedWrapper>
   );
