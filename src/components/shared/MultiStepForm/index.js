34b6ad45106f1d3125575b2b2fd632c66edc5556
Seemant Kamlapuri <seemantkamlapuri88@gmail.com>
2026-02-04 22:03:17 +0530

    feat: remove example configuration export from MultiStepForm

--- src/components/shared/MultiStepForm/index.js
+++ src/components/shared/MultiStepForm/index.js
@@ -7,6 +7,3 @@ export { default as SelectionCardLayout } from './layouts/SelectionCardLayout';
 export { default as SearchListLayout } from './layouts/SearchListLayout';
 export { default as AddressSelectionLayout } from './layouts/AddressSelectionLayout';
 export { default as TableSummaryLayout } from './layouts/TableSummaryLayout';
-
-// Example configuration
-export { createDispatchSteps } from './example-config';
